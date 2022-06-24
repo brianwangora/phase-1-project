74b9bca65bd783de4dfcae3bd836017c8823dd75
Brian Wangora <brianwangora@gmail.com>
2022-06-24 20:53:54 +0300

    Stats Table Complete

--- src/index.js
+++ src/index.js
@@ -1,42 +1,70 @@
-//Fetch Driver's data 
-function fetchData () {
-    fetch('https://brianwangora.github.io/phase-1-project/db.json')
-    .then(res => res.json())
-    .then(drivers => drivers.forEach(driver => displayDriverCode(driver)))
-}
-fetchData()
+//DOMContentLoaded
+document.addEventListener('DOMContentLoaded', () => {
 
-//Display Driver Codes on DOM
-function displayDriverCode(driver) {
-    let codeTag = document.createElement('div')
-    codeTag.id="driverCodes"
-    codeTag.innerHTML = 
-        `<p>
-            <span>${driver.code}</span>
-        </p>`
-    document.querySelector('#driver-codes').appendChild(codeTag)
-    codeTag.addEventListener('click', () => {(
-        displayDriverName(driver),
-        displayDriverTeam(driver),
-        displayDriverImage(driver)
-    )})    
-}
+    //Fetch Driver's data 
+    let fetchData= function () {
+        fetch('https://brianwangora.github.io/phase-1-project/db.json')
+        .then(res => res.json())
+        .then(drivers => drivers.forEach(driver => displayDriverCode(driver)))
+    }
+    fetchData()
 
-//Display Driver Name and Number on DOM
-function displayDriverName(driver) {
-    let driverName = document.querySelector('#name')
-    driverName.textContent=`${driver.name}, ${driver.number}`
-}
+    //Display Driver Codes on DOM
+    let displayDriverCode = function (driver) {
+        let codeTag = document.createElement('div')
+        codeTag.id="driverCodes"
+        codeTag.innerHTML = 
+            `<p>
+                <span>${driver.code}</span>
+            </p>`
+        document.querySelector('#driver-codes').appendChild(codeTag)
+        codeTag.addEventListener('click', () => {(
+            displayDriverName(driver), displayDriverTeam(driver), displayDriverImage(driver),
+            showRaceStarts(driver), showPolePositions (driver), showPodiums (driver), showWins(driver), showWorldChampionships(driver)
+        )})    
+    }
 
-//Display Driver Team
-function displayDriverTeam(driver) {
-    let driverTeam = document.querySelector('#team')
-    driverTeam.textContent=`${driver.team}`
-}
+    //Display Driver Name and Number on DOM
+    let displayDriverName = function (driver) {
+        let driverName = document.querySelector('#name')
+        driverName.textContent=`${driver.name}, ${driver.number}`
+    }
 
-//Display Driver Image
-function displayDriverImage(driver) {
-    let driverImage = document.querySelector('#image')
-    driverImage.src = `${driver.image}`
-    driverImage.alt= `${driver.name}`
- }
+    //Display Driver Team
+    let displayDriverTeam = function(driver) {
+        let driverTeam = document.querySelector('#team')
+        driverTeam.textContent=`${driver.team}`
+    }
+
+    //Display Driver Image
+    let displayDriverImage = function (driver) {
+        let driverImage = document.querySelector('#image')
+        driverImage.src = `${driver.image}`
+        driverImage.alt= `${driver.name}`
+    }
+
+//STATS Table
+    let showRaceStarts = function (driver) {
+        let raceStarts = document.querySelector('#starts-value')
+        raceStarts.textContent= `${driver.starts}`
+    }
+    let showPolePositions = function (driver) {
+        let polePositions = document.querySelector('#pole-value')
+        polePositions.textContent= `${driver.poles}`
+    }
+    let showPodiums = function (driver) {
+        let podiums = document.querySelector('#podiums-value')
+        podiums.textContent=`${driver.podiums}`
+    }
+    let showWins = function (driver) {
+        let wins = document.querySelector('#wins-value')
+        wins.textContent=`${driver.wins}`
+    }
+    let showWorldChampionships = function (driver) {
+        let championships = document.querySelector('#championships-value')
+        championships.textContent=`${driver.championships}`
+    }
+
+//Forms
+//Rating Form
+})
