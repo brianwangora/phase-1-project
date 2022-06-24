04f939b13996d06b176a10391b49cbf17edf45f7
Brian Wangora <brianwangora@gmail.com>
2022-06-24 19:39:28 +0300

    section 2 complete

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 function fetchData () {
     fetch('https://brianwangora.github.io/phase-1-project/db.json')
     .then(res => res.json())
-    .then(drivers => drivers.forEach(driver => driverCode(driver)))
+    .then(drivers => drivers.forEach(driver => displayDriverCode(driver)))
 }
 fetchData()
 
@@ -15,9 +15,28 @@ function displayDriverCode(driver) {
             <span>${driver.code}</span>
         </p>`
     document.querySelector('#driver-codes').appendChild(codeTag)
-    codeTag.addEventListener('click', () => {
-
-    })    
+    codeTag.addEventListener('click', () => {(
+        displayDriverName(driver),
+        displayDriverTeam(driver),
+        displayDriverImage(driver)
+    )})    
 }
 
-//
+//Display Driver Name and Number on DOM
+function displayDriverName(driver) {
+    let driverName = document.querySelector('#name')
+    driverName.textContent=`${driver.name}, ${driver.number}`
+}
+
+//Display Driver Team
+function displayDriverTeam(driver) {
+    let driverTeam = document.querySelector('#team')
+    driverTeam.textContent=`${driver.team}`
+}
+
+//Display Driver Image
+function displayDriverImage(driver) {
+    let driverImage = document.querySelector('#image')
+    driverImage.src = `${driver.image}`
+    driverImage.alt= `${driver.name}`
+ }
